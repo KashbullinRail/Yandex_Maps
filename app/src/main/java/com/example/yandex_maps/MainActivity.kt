d4a5da052be05a8de5f930604b3b55eaa298e306
KashbullinRail <kashbullinrail@gmail.com>
2023-04-24 19:04:15 +0300

    Initial commit

---
+++ app/src/main/java/com/example/yandex_maps/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.yandex_maps
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.appcompat.app.AppCompatActivity
+import com.example.yandex_maps.databinding.ActivityMainBinding
+import com.example.yandex_maps.ui.main.MainFragment
+import com.example.yandex_maps.ui.theme.Yandex_MapsTheme
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Yandex_MapsTheme {
+                AndroidViewBinding(ActivityMainBinding::inflate) {
+                    val fragment = container.getFragment<MainFragment>()
+                    //...
+                }
+            }
+        }
+    }
+}
