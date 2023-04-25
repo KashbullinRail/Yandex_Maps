bc31ec0fa04919192bd552d95b3582e3f35f86d4
KashbullinRail <kashbullinrail@gmail.com>
2023-04-25 19:56:39 +0300

    add poligon and circle

--- app/src/main/java/com/example/yandex_maps/ui/BlankFragment.kt
+++ app/src/main/java/com/example/yandex_maps/ui/BlankFragment.kt
@@ -9,25 +9,29 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import androidx.core.graphics.alpha
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.yandex_maps.R
 import com.example.yandex_maps.databinding.FragmentMainBinding
 import com.yandex.mapkit.Animation
 import com.yandex.mapkit.MapKitFactory
+import com.yandex.mapkit.geometry.Circle
+import com.yandex.mapkit.geometry.LinearRing
 import com.yandex.mapkit.geometry.Point
+import com.yandex.mapkit.geometry.Polygon
+import com.yandex.mapkit.layers.GeoObjectTapEvent
+import com.yandex.mapkit.layers.GeoObjectTapListener
 import com.yandex.mapkit.layers.ObjectEvent
-import com.yandex.mapkit.map.CameraPosition
-import com.yandex.mapkit.map.CompositeIcon
-import com.yandex.mapkit.map.IconStyle
-import com.yandex.mapkit.map.RotationType
+import com.yandex.mapkit.map.*
+import com.yandex.mapkit.map.Map
 import com.yandex.mapkit.mapview.MapView
 import com.yandex.mapkit.user_location.UserLocationLayer
 import com.yandex.mapkit.user_location.UserLocationObjectListener
 import com.yandex.mapkit.user_location.UserLocationView
 import com.yandex.runtime.image.ImageProvider
 
-class BlankFragment : Fragment(), UserLocationObjectListener {
+class BlankFragment : Fragment(), UserLocationObjectListener, GeoObjectTapListener, InputListener {
 
     private val viewModel by lazy {
         ViewModelProvider(this)[MainViewModel::class.java]
@@ -38,10 +42,13 @@ class BlankFragment : Fragment(), UserLocationObjectListener {
 
 
     private val TARGET_LOCATION = Point(55.763740752167465, 37.40582814075713)
+    private val CAMERA_TARGET = Point(59.951029, 30.317181)
     private val PERMISSIONS_REQUEST_FINE_LOCATION = 1
 
     private lateinit var mapView: MapView
     private lateinit var userLocationLayer: UserLocationLayer
+    private lateinit var sublayerManager: SublayerManager
+    private lateinit var mapObjects: MapObjectCollection
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -63,6 +70,35 @@ class BlankFragment : Fragment(), UserLocationObjectListener {
         super.onViewCreated(view, savedInstanceState)
 
         mapView = binding.mapview.findViewById(R.id.mapview)
+
+        mapView.map.addTapListener(this)
+        mapView.map.addInputListener(this)
+
+        binding.fabPoligons.setOnClickListener {
+            mapView.map.move(
+                CameraPosition(TARGET_LOCATION, 16.0f, 0.0f, 45.0f)
+            )
+
+            sublayerManager = mapView.map.sublayerManager
+            mapObjects = mapView.map.mapObjects
+
+            val circle = Circle(TARGET_LOCATION, 100f)
+            mapObjects.addCircle(circle, Color.BLUE, 2f, Color.MAGENTA.alpha)
+
+            val points = ArrayList<Point>()
+            points.add(Point(59.949911, 30.316560))
+            points.add(Point(59.949121, 30.316008))
+            points.add(Point(59.949441, 30.318132))
+            points.add(Point(59.949441, 30.319532))
+            points.add(Point(59.950075, 30.316915))
+            points.add(Point(59.949911, 30.316560))
+            points.add(Point(59.948511, 30.314560))
+            val polygon = Polygon(LinearRing(points), ArrayList())
+            val polygonMapObject: PolygonMapObject = mapObjects.addPolygon(polygon)
+            polygonMapObject.fillColor = 0x3300FF00
+            polygonMapObject.strokeWidth = 3.0f
+            polygonMapObject.strokeColor = Color.CYAN
+        }
 
         binding.fabStartPoint.setOnClickListener {
             mapView.getMap().move(
@@ -143,7 +179,7 @@ class BlankFragment : Fragment(), UserLocationObjectListener {
 
         pinIcon.setIcon(
             "icon",
-            ImageProvider.fromResource(requireContext(), R.drawable.icon),
+            ImageProvider.fromResource(requireContext(), R.drawable.search_result),
             IconStyle().setAnchor(PointF(0f, 0f))
                 .setRotationType(RotationType.ROTATE)
                 .setZIndex(0f)
@@ -166,6 +202,26 @@ class BlankFragment : Fragment(), UserLocationObjectListener {
     }
 
     override fun onObjectUpdated(p0: UserLocationView, p1: ObjectEvent) {
+    }
+
+    override fun onObjectTap(geoObjectTapEvent: GeoObjectTapEvent): Boolean {
+        val selectionMetadata: GeoObjectSelectionMetadata = geoObjectTapEvent
+            .getGeoObject()
+            .getMetadataContainer()
+            .getItem<GeoObjectSelectionMetadata>(GeoObjectSelectionMetadata::class.java)
+
+        if (selectionMetadata != null) {
+            mapView.map.selectGeoObject(selectionMetadata.id, selectionMetadata.layerId)
+        }
+
+        return selectionMetadata != null
+    }
+
+    override fun onMapTap(p0: Map, p1: Point) {
+        mapView.map.deselectGeoObject()
+    }
+
+    override fun onMapLongTap(p0: Map, p1: Point) {
     }
 
 }
